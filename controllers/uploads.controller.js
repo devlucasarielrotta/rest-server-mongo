008ae7af1543eb5b6ed682331ab28dd2e86873e0
Lucas Ariel Rotta <lucas.ariel.rotta@gmail.com>
2023-05-06 22:56:00 -0300

    Se agrega Cloudinary para la carga de archivos

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,11 +1,19 @@
-import {__dirname} from '../utils.js';
-import path from 'path';
 import {request, response} from 'express';
+import dotenv from 'dotenv'
+import {v2} from 'cloudinary';
+import path from 'path';
+import {__dirname} from '../utils.js';
 import {subirArchivo} from '../helpers/subir-archivo.js';
 
 import {usuarioModel, productoModel} from '../models/index.js';
 import {existsSync, unlinkSync} from 'fs';
-
+dotenv.config();
+v2.config({
+  cloud_name:process.env.CLOUDINARY_URL,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
+});
+console.log(process.env.CLOUDINARY_URL,process.env.CLOUDINARY_API_KEY,process.env.CLOUDINARY_API_SECRET)
 const cargarArchivo = async (req, res = response) => {
   try {
     const nombre = await subirArchivo(req.files, undefined, 'imgs');
@@ -67,6 +75,61 @@ const actualizarImagen = async (req, res = response) => {
   res.json(modelo);
 };
 
+const actualizarImagenCloudinary = async (req, res = response) => {
+  const {id, coleccion} = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case 'usuarios':
+      modelo = await usuarioModel.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe el usuario con id ${id}`,
+        });
+      }
+
+      break;
+
+    case 'productos':
+      modelo = await productoModel.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe el producto con id ${id}`,
+        });
+      }
+      break;
+
+    default:
+      return res.status(500).json({msg: 'Se me olvido validar esto'});
+  }
+
+  // limpiar imagenes previas
+ 
+  try {
+
+    if (modelo.img) {
+      
+      const nombreArr = modelo.img.split('/');
+      const nombre = nombreArr[ nombreArr.length - 1 ];
+      const [public_id] = nombre.split('.');    
+      
+      await v2.uploader.destroy(public_id);
+    }
+  } catch (error) {
+    return 'error', error;
+  }
+ 
+ 
+  const {tempFilePath} = req.files.archivo;
+  const {secure_url} = await v2.uploader.upload(tempFilePath)
+  
+  modelo.img = secure_url;
+  await modelo.save();
+   res.json({modelo})
+ 
+};
+
 const mostrarImagen = async (req, res = response) => {
   const {id, coleccion} = req.params;
 
@@ -115,4 +178,4 @@ const mostrarImagen = async (req, res = response) => {
   res.json(modelo);
 };
 
-export {cargarArchivo, actualizarImagen, mostrarImagen};
+export {cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary};
--- routes/uploads.router.js
+++ routes/uploads.router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {check} from 'express-validator';
-import { actualizarImagen, cargarArchivo, mostrarImagen } from '../controllers/uploads.controller.js';
+import { actualizarImagen, cargarArchivo, mostrarImagen , actualizarImagenCloudinary} from '../controllers/uploads.controller.js';
 import { validarCampos, validarArchivoSubir } from '../middlewares/index.js';
 import {coleccionesPermitidas} from '../helpers/db-validators.js';
 
@@ -13,7 +13,8 @@ router.put('/:coleccion/:id',[
     check('id','El id debe ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas( c, ['usuarios','productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary)
+// actualizarImagen
 
 router.get('/:coleccion/:id',[
     check('id','El id debe ser de mongo').isMongoId(),
